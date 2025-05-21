1a418021b18eabf352a3a4b9c6659926b992b750
aar-woo <awooyes@gmail.com>
2025-05-21 12:30:44 -1000

    Add header.

--- mbti-type-builder/src/App.js
+++ mbti-type-builder/src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header.tsx";
+import TypesSelect from "./components/TypesSelect.tsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>MBTI Type Builder</p>
+        <Header />
       </header>
+      <main>
+        <TypesSelect />
+      </main>
     </div>
   );
 }
---
+++ mbti-type-builder/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>MBTI TypeBuilder</h1>
+    </div>
+  );
+};
+
+export default Header;
