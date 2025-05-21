0ea67d1657420b8c2bf032cc318453b9e0d3cd9a
aar-woo <awooyes@gmail.com>
2025-05-21 13:07:11 -1000

    Add and refactor to use material ui components and styling.

--- mbti-type-builder/src/components/LetterType/LetterTypePiece.tsx
+++ mbti-type-builder/src/components/LetterType/LetterTypePiece.tsx
@@ -1,6 +1,23 @@
 import React from "react";
-import "./LetterTypePiece.css";
 import { LetterType } from "../../types/letterTypes.tsx";
+import styled from "@emotion/styled";
+import { Button, Card } from "@mui/material";
+
+const LetterButton = styled(Button)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  border: 4px solid;
+  border-radius: 10px;
+`;
+
+const LetterSpan = styled.span`
+  font-size: 4rem;
+  font-weight: bold;
+`;
 
 type LetterTypePieceProps = {
   letter: LetterType;
@@ -14,11 +31,9 @@ const LetterTypePiece = ({
   isActive = false,
 }: LetterTypePieceProps) => {
   return (
-    <button className="pieceContainer" style={{ borderColor: color }}>
-      <span className="letter" style={{ color: color }}>
-        {letter}
-      </span>
-    </button>
+    <LetterButton style={{ borderColor: color }}>
+      <LetterSpan style={{ color: color }}>{letter}</LetterSpan>
+    </LetterButton>
   );
 };
 
--- mbti-type-builder/src/components/TypesSelect.tsx
+++ mbti-type-builder/src/components/TypesSelect.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import TypeLetterPiece from "./LetterType/LetterTypePiece.tsx";
-import "./TypesSelect.css";
 import { LETTER_TYPES } from "../types/letterTypes.tsx";
 import COLORS from "../colors.tsx";
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+const LettersContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+`;
 
 const TypesSelect = () => {
   return (
-    <div className="letterPiecesContainer">
+    <LettersContainer>
       <TypeLetterPiece letter={LETTER_TYPES.I} color={COLORS.RED} />
-    </div>
+    </LettersContainer>
   );
 };
 
