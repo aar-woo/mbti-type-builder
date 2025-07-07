2038e1bb25ac0a76d6e7421501c0f1b9d0ce513e
aar-woo <awooyes@gmail.com>
2025-07-07 09:40:15 -1000

    Inline header text between two marquees. Give visual effect of icons passing through header text

--- mbti-type-builder/src/components/Header.tsx
+++ mbti-type-builder/src/components/Header.tsx
@@ -1,27 +1,48 @@
-import { styled, Typography } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 import Marquee from "react-fast-marquee";
 import { PERSONALITY_TYPES } from "../types/personalityTypes";
 
 const HeaderText = styled(Typography)`
-  padding: 1rem;
+  display: flex;
+  padding: 0.5rem;
+  white-space: nowrap;
 `;
+
+const typesArr = Object.values(PERSONALITY_TYPES);
+
+const personalityTypesHalfStart = typesArr
+  .slice(Math.floor(typesArr.length / 2), typesArr.length + 1)
+  .concat(typesArr.slice(0, Math.floor(typesArr.length / 2)));
+
+const personalityTypesOffset = typesArr
+  .slice(5, typesArr.length + 1)
+  .concat(typesArr.slice(0, 5));
 
 const Header = () => {
   return (
-    <Marquee direction="right" speed={20}>
-      {Object.values(PERSONALITY_TYPES).map((type, index) => {
-        if (index === 7) {
-          return <HeaderText variant="h3">MBTI Type Builder</HeaderText>;
-        }
-        return (
-          <img
-            src={`/images/${type}-avatar.png`}
-            style={{ width: "50px", margin: "0 30px 0 30px" }}
-          />
-        );
-      })}
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Marquee direction="right" speed={20}>
+        {Object.values(typesArr).map((type) => {
+          return (
+            <img
+              src={`/images/${type}-avatar.png`}
+              style={{ width: "32px", margin: "0 30px 0 30px" }}
+            />
+          );
+        })}
+      </Marquee>
       <HeaderText variant="h3">MBTI Type Builder</HeaderText>
-    </Marquee>
+      <Marquee direction="right" speed={20}>
+        {Object.values(personalityTypesOffset).map((type) => {
+          return (
+            <img
+              src={`/images/${type}-avatar.png`}
+              style={{ width: "30px", margin: "0 30px 0 30px" }}
+            />
+          );
+        })}
+      </Marquee>
+    </Box>
   );
 };
 
