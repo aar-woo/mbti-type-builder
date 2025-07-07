40d6bc860e9381364c6257333e1857779ae852b9
aar-woo <awooyes@gmail.com>
2025-07-07 08:55:48 -1000

    Updated header to be a marquee of revolving all the type icons.

--- mbti-type-builder/src/components/Header.tsx
+++ mbti-type-builder/src/components/Header.tsx
@@ -1,10 +1,28 @@
 import { styled, Typography } from "@mui/material";
+import Marquee from "react-fast-marquee";
+import { PERSONALITY_TYPES } from "../types/personalityTypes";
+
 const HeaderText = styled(Typography)`
   padding: 1rem;
 `;
 
 const Header = () => {
-  return <HeaderText variant="h3">MBTI TypeBuilder</HeaderText>;
+  return (
+    <Marquee direction="right" speed={20}>
+      {Object.values(PERSONALITY_TYPES).map((type, index) => {
+        if (index === 7) {
+          return <HeaderText variant="h3">MBTI Type Builder</HeaderText>;
+        }
+        return (
+          <img
+            src={`/images/${type}-avatar.png`}
+            style={{ width: "50px", margin: "0 30px 0 30px" }}
+          />
+        );
+      })}
+      <HeaderText variant="h3">MBTI Type Builder</HeaderText>
+    </Marquee>
+  );
 };
 
 export default Header;
