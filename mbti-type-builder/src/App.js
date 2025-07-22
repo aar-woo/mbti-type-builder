c9c5425398501b83803eae2869bdf4f908d1b419
aar-woo <awooyes@gmail.com>
2025-07-22 10:23:30 -1000

    Update app with basic react-router

--- mbti-type-builder/src/App.js
+++ mbti-type-builder/src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Header from "./components/Header.tsx";
+import TypeCardPage from "./pages/TypeCardPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <BrowserRouter className={"App"}>
+      <header className="app-header">
         <Header />
       </header>
-      <main>
-        <HomePage />
-      </main>
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="test" element={<TypeCardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
