68b0c2079da1fe6a6e4590b856a07ed980763438
aar-woo <awooyes@gmail.com>
2025-07-24 11:15:19 -1000

    Add button to navigate to home page from type page.

--- mbti-type-builder/src/pages/TypeCardPage.tsx
+++ mbti-type-builder/src/pages/TypeCardPage.tsx
@@ -1,12 +1,31 @@
+import { Box, Button, styled, Typography } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import PersonalityTypeInfo from "../components/PersonalityTypeInfo";
 import { PersonalityType } from "../types/personalityTypes";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+
+const ExploreTypesButton = styled(Button)`
+  text-decoration: none;
+  margin-top: 1rem;
+`;
 
 const TypeCardPage = () => {
   const { typeParam } = useParams();
+  let navigate = useNavigate();
 
   return (
-    <PersonalityTypeInfo type={typeParam.toUpperCase() as PersonalityType} />
+    <Box>
+      <ExploreTypesButton
+        color="primary"
+        startIcon={<ArrowBackIcon />}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <Typography>Explore other types</Typography>
+      </ExploreTypesButton>
+      <PersonalityTypeInfo type={typeParam.toUpperCase() as PersonalityType} />
+    </Box>
   );
 };
 
